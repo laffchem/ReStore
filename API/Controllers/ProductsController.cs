e23989c988166fd66ca8952852dac9768bb8521d
John Laffey <laffeychem@gmail.com>
2024-10-14 23:59:30 -0400

    End of Section 2

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<List<Product>>> GetProductsAsync()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return product;
+        }
+    }
+}
---
+++ API/Data/Migrations/20241015025804_FixEntities.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MPictureUrl",
+                table: "Products",
+                newName: "PictureUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PictureUrl",
+                table: "Products",
+                newName: "MPictureUrl");
+        }
+    }
+}
